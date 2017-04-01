73fe1ab4445fa9d21bfc694d50665409549e766a
csz <cszhi@qq.com>
2017-04-01 13:44:55 +0800

    migrations tuning

--- database/migrations/2017_02_16_101757_create_memus_table.php
+++ database/migrations/2017_02_16_101757_create_memus_table.php
@@ -19,7 +19,6 @@ class CreateMemusTable extends Migration
             $table->string('icon')->default('')->comment('图标');
             $table->string('slug')->default('')->comment('菜单对应的权限');
             $table->string('url')->default('')->comment('菜单链接地址');
-            $table->string('active')->default('')->comment('菜单高亮地址');
             $table->string('description')->default('')->comment('描述');
             $table->tinyInteger('sort')->default(0)->comment('排序');
             $table->timestamps();
